996c411a06943cbe9347a724e33df1e4d90f105c
enesbytmr <enesbytmr@gmail.com>
2024-03-17 04:15:39 +0300

    update for routing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
-import "./globals.css";
 import { cn } from "@/lib/utils";
+
+import "./globals.css";
 
 const IBMplex = IBM_Plex_Sans({ 
   subsets: ["latin"] ,
@@ -21,7 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn("font-IBMPlex antialiased", IBMplex.variable)}>{children}</body>
+      <body className={cn("font-IBMPlex antialiased", IBMplex.variable)}>
+        {children}
+        </body>
     </html>
   );
 }
