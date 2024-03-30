7335c6c2a7fa36fca5c5fb564dfd569609366bbb
enesbytmr <enesbytmr@gmail.com>
2024-03-30 17:15:45 +0300

    update required

--- src/lib/database/models/user.model.ts
+++ src/lib/database/models/user.model.ts
@@ -15,9 +15,9 @@ const UserSchema = new Schema({
     clerkId: {type: String, required: true, unique: true},
     email: {type: String, required: true , unique: true},
     username: {type: String, required: true, unique: true},
-    photo: {type: String},
-    firstName: {type: String},
-    lastName: {type: String},
+    photo: {type: String },
+    firstName: {type: String },
+    lastName: {type: String },
     planId: {type: Number, required: true ,default: '1'},
     creditBalance: {type: Number, required: true, default: 10 }
 })
